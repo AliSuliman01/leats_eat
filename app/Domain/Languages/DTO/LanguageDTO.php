93f8e21e9f92dfc9d696221ea71876c4ba3d29fb
Mohammad Tumeh <mhamadtumeh123123@gmail.com>
2022-08-28 11:51:38 +0300

    Authentication ...

--- app/Domain/Languages/DTO/LanguageDTO.php
+++ app/Domain/Languages/DTO/LanguageDTO.php
@@ -8,20 +8,20 @@ use Spatie\DataTransferObject\DataTransferObject;
 
 class LanguageDTO extends DataTransferObject
 {
-	/* @var integer|null */
+    /* @var integer|null */
     public $id;
     /* @var string|null */
-public $name;
-	/* @var string|null */
-public $abbrev;
+    public $name;
+    /* @var string|null */
+    public $abbrev;
 
 
     public static function fromRequest($request)
     {
         return new self([
-            'id'        =>  $request['id'] ?? null,
-            'name' 					=> $request['name'] ?? null ,
-			'abbrev' 					=> $request['abbrev'] ?? null ,
+            'id' => $request['id'] ?? null,
+            'name' => $request['name'] ?? null,
+            'abbrev' => $request['abbrev'] ?? null,
 
         ]);
     }
